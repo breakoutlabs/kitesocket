daf07e141aa24dec7706866c725f5b2ae5d0e240
breakoutlabs <154964128+breakoutlabs@users.noreply.github.com>
2024-09-10 13:50:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,60 @@ app.use(cors());
 var KiteTicker = require("kiteconnect").KiteTicker;
 
 app.get("/kite/stream", (req, res) => {
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+var KiteTicker = require("kiteconnect").KiteTicker;
+
+app.get("/kite/stream", (req, res) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+  res.flushHeaders();
+
+  const tokens = req.query.tokens ? req.query.tokens.split(",").map(Number) : [];
+
+  var ticker = new KiteTicker({
+    api_key: "x6uczl4asdrgqdt8",
+    access_token: "1lNjSz094r0yStAkY2nHrDtLw0qb5e0u",
+  });
+
+  ticker.connect();
+  ticker.on("ticks", onTicks);
+  ticker.on("connect", subscribe);
+
+  function onTicks(ticks) {
+    //console.log("Ticks", ticks);
+    res.write(`data: ${JSON.stringify(ticks)}\n\n`);
+  }
+
+  function subscribe() {
+    if (tokens.length > 0) {
+      ticker.subscribe(tokens);
+      ticker.setMode(ticker.modeFull, tokens);
+    } else {
+      console.log("No tokens provided for subscription");
+    }
+  }
+
+  req.on("close", () => {
+    console.log("Client disconnected");
+    // ticker.disconnect();
+    // res.end();
+  });
+  //   ticker.connect();
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
   const tokens = req.query.tokens ? req.query.tokens.split(",").map(Number) : [];
 
   var ticker = new KiteTicker({
